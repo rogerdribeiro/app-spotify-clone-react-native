ddf921b9c34f4767ab192b88e7c6353677746f0f
Rogerd Ribeiro <rogerd.ribeiro@aluno.ufop.edu.br>
2019-02-22 09:29:23 -0300

    config store -> ducks e sagas

--- src/index.js
+++ src/index.js
@@ -1,31 +1,11 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import store from './store';
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+const App = () => (
+  <Provider store={store}>
+    <View />
+  </Provider>
+);
+export default App;
